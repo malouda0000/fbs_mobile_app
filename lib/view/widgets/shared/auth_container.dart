08b6d3d0bea36e78655f3ea7b96ab372f4c2467b
malouda0000 <hamam.hamza85@gmail.com>
2022-11-13 20:29:09 +0200

    fixed the auth background image

--- lib/view/widgets/shared/auth_container.dart
+++ lib/view/widgets/shared/auth_container.dart
@@ -13,89 +13,114 @@ class AuthContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // double mySuperX = 1000 - Get.width;
     return Scaffold(
       body: Stack(
         children: [
           Positioned(
-            left: -Get.width * .5,
-            top: -600,
-            width: Get.width * 2,
-            height: Get.width * 2,
-            child: Center(
-              child: Container(
-                padding: const EdgeInsets.only(bottom: 10),
-                decoration: const BoxDecoration(
-                  // boxShadow: [
-                  //   BoxShadow(
-                  //     color: AppColor.kGrayColor,
-                  //     blurRadius: .1,
-                  //   )
-                  // ],
-                  color: AppColor.kPrimaryColor,
-                  shape: BoxShape.circle,
-                ),
-                height: Get.width * 2,
-                width: Get.width * 2,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Container(
-                      width: 150,
-                      height: 150,
-                      // logo container
-                      // clipBehavior: Clip.hardEdge,
-                      // padding: const EdgeInsets.all(5),
+            left: -((1000 - Get.width) / 2),
+            // left: -mySuperX / 2,
+            top: -790,
 
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        // color: AppColor.kColorFour,
-                        color: Theme.of(context).scaffoldBackgroundColor,
-                      ),
-                      child: const Image(
-                        // fit: BoxFit.,
-                        width: 110,
-                        height: 110,
-                        color: AppColor.kColorThree,
-                        image: AssetImage(
-                          AppImages.mainIcon,
-                        ),
+            // left: -Get.width,
+            // top: -600,
+
+            // width: Get.width * 2,
+            // height: Get.width * 2,
+            width: 1000,
+            height: 1000,
+            child: Container(
+              // alignment: Alignment.bottomLeft,
+              alignment: Alignment.center,
+              // padding: const EdgeInsets.only(bottom: 10),
+              decoration: const BoxDecoration(
+                // boxShadow: [
+                //   BoxShadow(
+                //     color: AppColor.kGrayColor,
+                //     blurRadius: .1,
+                //   )
+                // ],
+                color: AppColor.kPrimaryColor,
+                // color: Colors.yellow,
+                shape: BoxShape.circle,
+              ),
+              height: 1000,
+              width: 1000,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const Spacer(),
+                  Container(
+                    // logo container, white container with the logo inside it
+
+                    width: 150,
+                    height: 150,
+                    // clipBehavior: Clip.hardEdge,
+                    // padding: const EdgeInsets.all(5),
+
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      // color: AppColor.kColorFour,
+                      color: Theme.of(context).scaffoldBackgroundColor,
+                    ),
+                    child: const Image(
+                      // fit: BoxFit.,
+                      width: 110,
+                      height: 110,
+                      color: AppColor.kColorThree,
+                      image: AssetImage(
+                        AppImages.mainIcon,
                       ),
                     ),
-                    emptySpace,
-                    emptySpace,
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(
-                theDefaultPadding,
-              ),
-              child: Column(
-                children: [
-                  // SizedBox(
-                  //   height: 300,
-                  // ),
-
-                  const SizedBox(
-                    // height: Get.height * .35,
-                    height: 220,
                   ),
-                  Expanded(
-                    // the body
-                    child: SingleChildScrollView(child: mainWidget),
-                  ),
-
-                  // BigTitleBuilder(theTitle: 'sign in'),
+                  emptySpace,
+                  emptySpace,
                 ],
               ),
             ),
           ),
+          AuthBody(mainWidget: mainWidget),
         ],
+      ),
+    );
+  }
+}
+
+class AuthBody extends StatelessWidget {
+  const AuthBody({
+    Key? key,
+    required this.mainWidget,
+  }) : super(key: key);
+
+  final Widget mainWidget;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(
+          theDefaultPadding,
+        ),
+        child: Column(
+          children: [
+            // SizedBox(
+            //   height: 300,
+            // ),
+
+            const SizedBox(
+              // height: Get.height * .35,
+              height: 210,
+            ),
+            Expanded(
+              // the body
+              child: SingleChildScrollView(child: mainWidget),
+            ),
+
+            // BigTitleBuilder(theTitle: 'sign in'),
+          ],
+        ),
       ),
     );
   }
